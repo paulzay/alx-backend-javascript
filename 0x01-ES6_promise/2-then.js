2135a601aed80ebe631e40c45391ffb6aa68d942
Paul Ogolla <paulotieno2@gmail.com>
2024-08-07 16:05:11 +0300

    2-then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,5 +2,8 @@ export default function handleResponseFromAPI(promise) {
   return promise.then(() => ({
     status: 200,
     body: 'success',
-  })).catch(() => new Error('The fake API is not working currently'));
+  })).catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
