88c5656b2cef36a99626059de62936b5d4caf220
Paul Ogolla <paulotieno2@gmail.com>
2024-08-08 23:05:22 +0300

    2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -4,27 +4,33 @@ export default class HolbertonCourse {
     this._length = length;
     this._students = students;
   }
+
   get name() {
     return this._name;
   }
+
   set name(newName) {
     if (typeof newName !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = newName;
   }
+
   get length() {
     return this._length;
   }
+
   set length(newLength) {
     if (typeof newLength !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = newLength;
   }
+
   get students() {
     return this._students;
   }
+
   set students(newStudents) {
     if (!Array.isArray(newStudents)) {
       throw new TypeError('Students must be an array');
