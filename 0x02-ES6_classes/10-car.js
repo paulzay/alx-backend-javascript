6a8994eb348110e98365fa66d95d509682962b6a
Paul Ogolla <paulotieno2@gmail.com>
2024-08-09 00:10:03 +0300

    10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -5,6 +5,31 @@ export default class Car {
     this.color = color;
 
   }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
   cloneCar() {
     return new Car(this.motor, this.brand, this.color);
   }
