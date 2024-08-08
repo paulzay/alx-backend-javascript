3d27a8e97ba1a14888a92d1418e20bb00d37f4c9
Paul Ogolla <paulotieno2@gmail.com>
2024-08-08 16:43:25 +0300

    2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -8,18 +8,27 @@ class HolbertonCourse {
     return this._name;
   }
   set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = newName;
   }
   get length() {
     return this._length;
   }
   set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
     this._length = newLength;
   }
   get students() {
     return this._students;
   }
   set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw new TypeError('Students must be an array');
+    }
     this._students = newStudents;
   }
 }
