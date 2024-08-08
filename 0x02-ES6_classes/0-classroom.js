4012c8f46b86097e61df25f722761d1225170955
Paul Ogolla <paulotieno2@gmail.com>
2024-08-08 16:46:17 +0300

    0

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -2,16 +2,4 @@ export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
-
-  get maxStudentsSize() {
-    return this._maxStudentsSize;
-  }
-
-  set maxStudentsSize(newSize) {
-    if (typeof newSize === 'number') {
-      this._maxStudentsSize = newSize;
-    } else {
-      throw new TypeError('maxStudentsSize must be a Number');
-    }
-  }
 }
