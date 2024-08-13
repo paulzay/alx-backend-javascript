96d4b06127e2cd30d55f59f6a76a5983612e3199
Paul Ogolla <paulotieno2@gmail.com>
2024-08-13 22:03:03 +0300

    2,3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getStudentIdsSum(students) {
-	return students.reduce((acc, student) => acc + student.id, 0);
+  return students.reduce((acc, student) => acc + student.id, 0);
 }
