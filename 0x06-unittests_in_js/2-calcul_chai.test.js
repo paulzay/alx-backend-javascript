1e54e0f218474af10cb3fd32389e66fd5dc808e5
Paul Ogolla <paulotieno2@gmail.com>
2024-10-03 23:30:20 +0300

    3

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,29 +1,28 @@
-import * as chai from 'chai';
+const chai = require('chai');
+const calculateNumber = require('./1-calculateNumber');
 
-import { calcul } from '../1-calcul';
+const { expect } = chai;
 
-const expect = chai.expect;
-
-describe('calcul', function () {
+describe('calculateNumber', function () {
   it('should return 4', function () {
-    expect(calcul('SUM', 1, 3)).to.equal(4);
+    expect(calculateNumber('SUM', 1, 3)).to.equal(4);
   });
   it('should return 5', function () {
-    expect(calcul('SUM', 1, 3.7)).to.equal(5);
+    expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
   });
   it('should return 5', function () {
-    expect(calcul('SUM', 1.2, 3.7)).to.equal(5);
+    expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
   });
   it('should return 6', function () {
-    expect(calcul('SUM', 1.5, 3.7)).to.equal(6);
+    expect(calculateNumber('SUM', 1.5, 3.7)).to.equal(6);
   });
   it('should return -2', function () {
-    expect(calcul('SUBTRACT', 1.5, 3.7)).to.equal(-2);
+    expect(calculateNumber('SUBTRACT', 1.5, 3.7)).to.equal(-2);
   });
   it('should return 0', function () {
-    expect(calcul('SUBTRACT', 1.5, 3.7)).to.equal(-2);
+    expect(calculateNumber('SUBTRACT', 1.5, 3.7)).to.equal(-2);
   });
   it('should return Error', function () {
-    expect(calcul('DIVIDE', 1.5, 0)).to.equal('Error');
+    expect(calculateNumber('DIVIDE', 1.5, 0)).to.equal('Error');
   });
 });
---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,16 @@
+var sinon = require("sinon");
+
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', function () {
+  it('should return 10', function () {
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const consoleSpy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 10);
+    expect(calculateNumberStub.calledWith('SUM', 100, 10)).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 10')).to.be.true;
+    calculateNumberStub.restore();
+    consoleSpy.restore();
+  });
+});
